64d0ab34a92c8ecbbe3df5c3825a9479ce66193e
Werefox <jack@wrightus.com>
2023-07-23 09:52:31 -0400

    Added file for the Screen class

---
+++ Runtime/Screen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Foxthorne.FoxScreens
+{
+    public class Screen : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
